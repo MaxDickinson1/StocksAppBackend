e98b9e879a1013747c28f2fb5ac4f40f70e2bc50
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-18 15:21:51 -0700

    user auth

--- server.js
+++ server.js
@@ -3,29 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+
 
 const cors = require('cors');
 
 // Allow requests from any origin
 app.use(cors());
-
-
-
-// Allow requests from a specific domain
-const allowedOrigins = ['https://stately-salmiakki-6c7124.netlify.app'];
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-};
-
-// Use the cors middleware
-app.use(cors(corsOptions));
 
 
 // Connect to MongoDB
