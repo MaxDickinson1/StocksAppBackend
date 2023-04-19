2c01aa1d63667038472dde11fa9c59512058ab8b
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-19 02:33:22 -0700

    cors

--- server.js
+++ server.js
@@ -7,14 +7,23 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-  origin: '*', 
+  origin: function (origin, callback) {
+    const allowedOrigins = ['https://stately-salmiakki-6c7124.netlify.app', 'http://localhost:3000'];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
-
 app.use(cors(corsOptions));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
