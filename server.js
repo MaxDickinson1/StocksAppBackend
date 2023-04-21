2d3f8efe2612604d353bae2ae5a48ad5f324fcc2
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-21 14:56:11 -0700

    back to local host

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'https://stately-salmiakki-6c7124.netlify.app',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   credentials: true,
   allowedHeaders: [
